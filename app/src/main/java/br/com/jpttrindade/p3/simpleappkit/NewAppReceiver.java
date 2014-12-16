82e5003b1f97b89d367a6133e1e51e7707fe6dca
jpttrindade <tenoriojpaulo@gmail.com>
2014-12-16 17:34:17 -0300

    commit inicial com a base do projeto

---
+++ app/src/main/java/br/com/jpttrindade/p3/simpleappkit/NewAppReceiver.java
@@ -0,0 +1,15 @@
+package br.com.jpttrindade.p3.simpleappkit;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by joaotrindade on 18/11/14.
+ */
+public class NewAppReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
